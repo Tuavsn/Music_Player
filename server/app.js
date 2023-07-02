1917db23d9dde48d26f8c4532aa6a3bab7ebc911
Tuavsn <hoctuavsn@gmail.com>
2023-07-02 11:38:56 +0700

    seccond replace

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const cors = require("cors")
 app.use(cors({origin: true}))
 app.use(express.json())
 
-const userRoute = require("./routes/auth")
-app.use("/api/users", userRoute)
+//const userRoute = require("./routes/auth")
+//app.use("/api/users", userRoute)
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000")
